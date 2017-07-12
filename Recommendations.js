2be517026dadf358ec24200cc0ddc16b3eca2f49
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 07:25:13 -0700

    Change Postman token and responseJson to string.

--- Recommendations.js
+++ Recommendations.js
@@ -20,7 +20,7 @@ export default class Recommendations extends Component {
         "headers": {
           "authorization": "Bearer ac70c6f9f54d7cfce3c76c5b1cc93a53fe98de22459265c5e97a32d8d46db02220e12c4bb701ee3d",
           "cache-control": "no-cache",
-          "postman-token": "5f9fe8b9-7aac-cea0-af81-91c4b5cf74df"
+          "postman-token": "41600385-26df-cd4e-5a26-eb71c0efd6c4"
         }
       };      
       let response = await fetch('https://api.npr.org/listening/v2/recommendations', options);
@@ -28,7 +28,7 @@ export default class Recommendations extends Component {
       let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
       this.setState({
         isLoading: false,
-        dataSource: ds.cloneWithRows(responseJson.attributes.affiliations),
+        dataSource: ds.cloneWithRows(responseJson.toString()),
       }, function() {
         // do something with new state
       });
