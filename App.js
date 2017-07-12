b7c0eba32ef28047bfc4376f6451fb775bd6f6a7
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-11 22:54:12 -0700

    Set up App component.

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+
+const reactNativeExample = StackNavigator({
+  Home: { screen: HomeScreen },
+});
+
+AppRegistry.registerComponent('reactNativeExample', () => reactNativeExample);
