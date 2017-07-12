68700803468f3b25e0bc55c276e0ab7c5b61446d
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 07:29:31 -0700

    Create Play Screen component.

---
+++ Play.js
@@ -0,0 +1,292 @@
+let mockRecs = {
+    "version": "1.0",
+    "href": "https://api.npr.org/identity/v2/user",
+    "attributes": {
+        "id": "26666429",
+        "email": "joseph.sc.luk@gmail.com",
+        "firstName": "Joseph",
+        "lastName": "Luk",
+        "cohort": {
+            "id": "B",
+            "name": "Testing a lower Up First floor",
+            "public": "230"
+        },
+        "organizations": [
+            {
+                "id": "151",
+                "displayName": "KQED Public Media",
+                "call": "KQED",
+                "city": "San Francisco",
+                "logo": "https://media.npr.org/images/stations/nprone_logos/kqed.png",
+                "smallLogo": "https://media.npr.org/images/stations/logos/kqed.gif",
+                "donationUrl": "http://www.kqed.org/pledge/index.jsp?__nprid=true&scid=46623&utm_source=NPROne&utm_medium=app-referral&utm_content=20170712&utm_campaign=NPRdonate",
+                "notif_org": {
+                    "ios": "/topics/prod_org_IOS_151",
+                    "default": "/topics/prod_org_151"
+                },
+                "membership": {
+                    "memberType": "Non-Member"
+                },
+                "donation": {
+                    "paymentGatewayToken": "I73GLq9eFC854Eo4zSJmpl6wOJs"
+                }
+            }
+        ],
+        "affiliations": [
+            {
+                "id": "344098539",
+                "title": "Wait Wait... Don't Tell Me!",
+                "href": "https://api.npr.org/listening/v2/aggregation/344098539/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_344098539",
+                    "default": "/topics/prod_followed_344098539"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_344098539",
+                    "default": "/topics/prod_rated_344098539"
+                },
+                "rating": 0.3,
+                "daysSinceLastListen": 100
+            },
+            {
+                "id": "399954056",
+                "title": "Nerdette Podcast",
+                "href": "https://api.npr.org/listening/v2/aggregation/399954056/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_399954056",
+                    "default": "/topics/prod_followed_399954056"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_399954056",
+                    "default": "/topics/prod_rated_399954056"
+                },
+                "rating": 0.2,
+                "daysSinceLastListen": 256
+            },
+            {
+                "id": "432285393",
+                "title": "The California Report",
+                "href": "https://api.npr.org/listening/v2/aggregation/432285393/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_432285393",
+                    "default": "/topics/prod_followed_432285393"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_432285393",
+                    "default": "/topics/prod_rated_432285393"
+                },
+                "rating": 0.3,
+                "daysSinceLastListen": 113
+            },
+            {
+                "id": "447248267",
+                "title": "The Leap",
+                "href": "https://api.npr.org/listening/v2/aggregation/447248267/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_447248267",
+                    "default": "/topics/prod_followed_447248267"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_447248267",
+                    "default": "/topics/prod_rated_447248267"
+                },
+                "rating": 0.9,
+                "daysSinceLastListen": 75
+            },
+            {
+                "id": "452538045",
+                "title": "Freakonomics Radio",
+                "href": "https://api.npr.org/listening/v2/aggregation/452538045/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_452538045",
+                    "default": "/topics/prod_followed_452538045"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_452538045",
+                    "default": "/topics/prod_rated_452538045"
+                },
+                "rating": 0.77,
+                "daysSinceLastListen": 100
+            },
+            {
+                "id": "510019",
+                "title": "All Songs Considered",
+                "href": "https://api.npr.org/listening/v2/aggregation/510019/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510019",
+                    "default": "/topics/prod_followed_510019"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510019",
+                    "default": "/topics/prod_rated_510019"
+                },
+                "rating": 0.3,
+                "daysSinceLastListen": 62
+            },
+            {
+                "id": "510289",
+                "title": "Planet Money",
+                "href": "https://api.npr.org/listening/v2/aggregation/510289/recommendations",
+                "following": true,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510289",
+                    "default": "/topics/prod_followed_510289"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510289",
+                    "default": "/topics/prod_rated_510289"
+                },
+                "rating": 1,
+                "daysSinceLastListen": 75
+            },
+            {
+                "id": "510298",
+                "title": "TED Radio Hour",
+                "href": "https://api.npr.org/listening/v2/aggregation/510298/recommendations",
+                "following": true,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510298",
+                    "default": "/topics/prod_followed_510298"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510298",
+                    "default": "/topics/prod_rated_510298"
+                },
+                "rating": 0.88,
+                "daysSinceLastListen": 85
+            },
+            {
+                "id": "510308",
+                "title": "Hidden Brain",
+                "href": "https://api.npr.org/listening/v2/aggregation/510308/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510308",
+                    "default": "/topics/prod_followed_510308"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510308",
+                    "default": "/topics/prod_rated_510308"
+                },
+                "rating": 0.65,
+                "daysSinceLastListen": 184
+            },
+            {
+                "id": "510310",
+                "title": "NPR Politics Podcast",
+                "href": "https://api.npr.org/listening/v2/aggregation/510310/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510310",
+                    "default": "/topics/prod_followed_510310"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510310",
+                    "default": "/topics/prod_rated_510310"
+                },
+                "rating": 0.9,
+                "daysSinceLastListen": 296
+            },
+            {
+                "id": "510312",
+                "title": "Code Switch",
+                "href": "https://api.npr.org/listening/v2/aggregation/510312/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510312",
+                    "default": "/topics/prod_followed_510312"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510312",
+                    "default": "/topics/prod_rated_510312"
+                },
+                "rating": 0.58,
+                "daysSinceLastListen": 256
+            },
+            {
+                "id": "510313",
+                "title": "How I Built This",
+                "href": "https://api.npr.org/listening/v2/aggregation/510313/recommendations",
+                "following": true,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510313",
+                    "default": "/topics/prod_followed_510313"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510313",
+                    "default": "/topics/prod_rated_510313"
+                },
+                "rating": 0.91,
+                "daysSinceLastListen": 135
+            },
+            {
+                "id": "510318",
+                "title": "Up First",
+                "href": "https://api.npr.org/listening/v2/aggregation/510318/recommendations",
+                "following": true,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510318",
+                    "default": "/topics/prod_followed_510318"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510318",
+                    "default": "/topics/prod_rated_510318"
+                },
+                "rating": 0.95,
+                "daysSinceLastListen": 83
+            },
+            {
+                "id": "510889078",
+                "title": "Q'ed Up",
+                "href": "https://api.npr.org/listening/v2/aggregation/510889078/recommendations",
+                "following": true,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_510889078",
+                    "default": "/topics/prod_followed_510889078"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_510889078",
+                    "default": "/topics/prod_rated_510889078"
+                },
+                "rating": 0.78,
+                "daysSinceLastListen": 137
+            },
+            {
+                "id": "512508710",
+                "title": "Civics 101",
+                "href": "https://api.npr.org/listening/v2/aggregation/512508710/recommendations",
+                "following": false,
+                "notif_following": {
+                    "ios": "/topics/prod_followed_IOS_512508710",
+                    "default": "/topics/prod_followed_512508710"
+                },
+                "notif_rated": {
+                    "ios": "/topics/prod_rated_IOS_512508710",
+                    "default": "/topics/prod_rated_512508710"
+                },
+                "rating": 0.65,
+                "daysSinceLastListen": 80
+            }
+        ],
+        "totalListeningTime": "135879"
+    },
+    "items": [],
+    "links": {
+        "organizations": [
+            {
+                "content-type": "application/json",
+                "href": "https://api.npr.org/listening/v2/organizations/151/recommendations"
+            }
+        ]
+    },
+    "errors": []
+};
+
+
+
