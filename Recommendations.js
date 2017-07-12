7a0e259ad9af14dc8fecb2a958363f6f2b1e7890
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 12:48:15 -0700

    Move init variable.

--- Recommendations.js
+++ Recommendations.js
@@ -12,20 +12,20 @@ class Recommendations extends Component {
 
   async componentDidMount() {
     try {
-      let myRequest = new Request ('https://api.npr.org/listening/v2/recommendations', myInit)
       let myInit = {
         "method": "GET",
         "headers": {
           "authorization": "Bearer ec47b4a995772bdf0f998e41f100114a49fb773e9a4e59748113594070a3f5c6466ae0f42a20a634&state=test123&uidt=1499887503",
         },
         "mode": "cors"
-      };      
+      }; 
+      let myRequest = new Request ('https://api.npr.org/listening/v2/recommendations', myInit)     
       let response = await fetch(myRequest);
       let responseJson = await response.json();
       let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
       this.setState({
         isLoading: false,
-        dataSource: ds.cloneWithRows(responseJson.toString()),
+        dataSource: ds.cloneWithRows(responseJson),
       }, function() {
         // do something with new state
       });
@@ -47,7 +47,7 @@ class Recommendations extends Component {
       <View style={{flex: 1, paddingTop: 20}}>
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
+          renderRow={(rowData) => <Text>{rowData.items}</Text>}
         />
       </View>
     );
