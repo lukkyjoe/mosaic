b1f3c82b1bde687bafd10b8bd62c9f5d9becec0f
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 01:59:45 -0700

    Change wording to Recommendations.

--- Recommendations.js
+++ Recommendations.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, ListView, Text, View } from 'react-native';
 
-export default class Movies extends Component {
+export default class Recommendations extends Component {
   constructor(props) {
     super(props);
     this.state = {
