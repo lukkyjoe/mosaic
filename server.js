6991b0bcbd2c5c85f82f56fde0da55078a76ab5f
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 23:24:47 -0700

    Implement auth token retrieval and Mongoose save.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const https = require('https');
-// var Save = require('./AsyncStorage');
+const request = require('request');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config')
 
 mongoose.connect('mongodb://localhost/mosaic');
 let db = mongoose.connection;
@@ -17,7 +17,6 @@ let Token = mongoose.model('Token', tokenSchema);
 
 app.use(bodyParser.json())
 
-// console.log(Native.AsyncStorage) 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -25,43 +24,28 @@ app.get('/', function (req, res) {
 app.get('/oauth2/callback', function (req, res) {
   console.log('req.query.code here ------------', req.query.code);
   res.send('callback url has been hit');
-  // https.get()
+  let authCode = req.query.code.toString();
+  request.post('https://api.npr.org/authorization/v2/token', {
+    form: {
+      grant_type: 'authorization_code',
+      client_id: config.appID,
+      client_secret: config.appSecret,
+      code: authCode,
+      redirect_uri: 'http://localhost:3000/oauth2/callback'
+    },
+    json: true
+  }, (err, res, body) => {
+      console.log('THIS SHOULD BE THE TOKEN BODY', body)
+      Token.create({token: body.access_token}, (err, tokenData) => {
+        if (err) {console.error(err)}
+        console.log(tokenData);
+      })
+    });
   let aString = JSON.stringify(req.query);
-  Token.create({token: aString}, (err, tokenData) => {
-    if (err) {console.error(err)}
-    console.log(tokenData);
-  })
+
   // asyncStorage.setItem('tokenInfo', 123);
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Mosaic NPR takehome app listening on port 3000!')
 })
-
-
-// var options = {
-//   "method": "GET",
-//   "hostname": "api.npr.org",
-//   "port": null,
-//   "path": "/identity/v2/user?access_token=ac70c6f9f54d7cfce3c76c5b1cc93a53fe98de22459265c5e97a32d8d46db02220e12c4bb701ee3d",
-//   "headers": {
-//     "authorization": "Bearer ac70c6f9f54d7cfce3c76c5b1cc93a53fe98de22459265c5e97a32d8d46db02220e12c4bb701ee3d",
-//     "cache-control": "no-cache",
-//     "postman-token": "41600385-26df-cd4e-5a26-eb71c0efd6c4"
-//   }
-// };
-
-// var req = https.request(options, function (res) {
-//   var chunks = [];
-
-//   res.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
-
-//   res.on("end", function () {
-//     var body = Buffer.concat(chunks);
-//     console.log(body.toString());
-//   });
-// });
-
-// req.end();
