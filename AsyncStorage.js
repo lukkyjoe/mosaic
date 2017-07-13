70c4bb8bc5718caba4fefa67e811c2a434df0415
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 23:24:07 -0700

    Comment out asyncstorage.

--- AsyncStorage.js
+++ AsyncStorage.js
@@ -1,13 +1,13 @@
-const native = require('react-native');
-let {
-  Text,
-  View, 
-  Button, 
-  WebView, 
-  AsyncStorage
-} = native;
+// const native = require('react-native');
+// let {
+//   Text,
+//   View, 
+//   Button, 
+//   WebView, 
+//   AsyncStorage
+// } = native;
 
-modules.export = native;
-modules.export.saveSomething = function (something) {
-  AsyncStorage.setItem('someKey', something);
-}
+// modules.export = native;
+// modules.export.saveSomething = function (something) {
+//   AsyncStorage.setItem('someKey', something);
+// }
