fabf68d5f8fe80190537692d2bb33e55b5c17c72
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-12 20:42:53 -0700

    Test AsyncStorage file.

---
+++ AsyncStorage.js
@@ -0,0 +1,13 @@
+const native = require('react-native');
+let {
+  Text,
+  View, 
+  Button, 
+  WebView, 
+  AsyncStorage
+} = native;
+
+modules.export = native;
+modules.export.saveSomething = function (something) {
+  AsyncStorage.setItem('someKey', something);
+}
